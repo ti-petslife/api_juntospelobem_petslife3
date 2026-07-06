552c1324a9432f6d5e55b5966d01fbfb75dcdc48
YesleMB <wesleymb2002@gmail.com>
2026-07-06 17:09:05 -0300

    ajuste

--- src/main/java/com/juntospelobem/pets/config/CorsConfig.java
+++ src/main/java/com/juntospelobem/pets/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("https://api-juntospelobem-petslife3.onrender.com") 
+                .allowedOrigins("http://localhost:5173", "https://juntospelobem.petslife.vet.br")               
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") 
                 .allowCredentials(true); 
