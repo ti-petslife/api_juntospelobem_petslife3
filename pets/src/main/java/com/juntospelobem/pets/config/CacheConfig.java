aa41b02b35ed96f6590848419e51babdf216e304
YesleMB <wesleymb2002@gmail.com>
2026-08-20 11:22:06 -0300

    ajuste nos codigos

--- pets/src/main/java/com/juntospelobem/pets/config/CacheConfig.java
+++ pets/src/main/java/com/juntospelobem/pets/config/CacheConfig.java
@@ -1,16 +1,23 @@
-
 package com.juntospelobem.pets.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("otpCache", "cardsCache");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("otpCache", "cardsCache");
+
+       
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .maximumSize(1000)); 
+        return cacheManager;
     }
 }
